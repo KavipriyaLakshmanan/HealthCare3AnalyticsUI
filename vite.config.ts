edd22baf8900f4b0bca8415d00346c2736f54576
KavipriyaLakshmanan <Kavipriya.L@3analytics.com>
2024-12-21 13:51:20 +0530

    changes for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/HealthCare3AnalyticsUI/', // Replace <repository-name> with your repo's name
   plugins: [react()],
-})
+
+});
